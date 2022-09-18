8e68a2d4f28d0320426282e82e20e01f75ca367f
Tejas Agawane <tejas.agawane@ey.com>
2022-09-18 18:50:43 +0530

    Axios url update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
-  //   baseURL:
-  //     "https://cors-everywhere.herokuapp.com/http://subhashshoebaramati-env.eba-iwauktpw.us-west-2.elasticbeanstalk.com",
+  // baseURL: "http://localhost:8080",
+    baseURL:
+      "https://cors-everywhere.herokuapp.com/http://springbootmysqlcrudshop-env.eba-n3f7imtv.us-west-2.elasticbeanstalk.com",
 });
