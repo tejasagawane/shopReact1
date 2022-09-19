ba72bb9e5ab98a68895e767072c24a3186162523
tejasagawane <92745176+tejasagawane@users.noreply.github.com>
2022-09-19 16:29:39 +0530

    Update axios.js
    Shreenath shoes URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://localhost:8080",
     baseURL:
-      "https://cors-everywhere.herokuapp.com/http://springbootmysqlcrudshop-env.eba-n3f7imtv.us-west-2.elasticbeanstalk.com",
+      "https://cors-everywhere.herokuapp.com/http://shopspringbootshreenathshoes-env.eba-g9yapqmv.us-west-2.elasticbeanstalk.com/",
 });
